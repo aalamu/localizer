373c67cf123faffc1911216d8d9a9e42281c97b5
Yusuf Alamu Musa <alamu@fleencorp.com>
2025-05-17 16:58:07 +0100

    add LocalizedResponse interface for general use

--- src/main/java/com/fleencorp/localizer/model/response/ApiResponse.java
+++ src/main/java/com/fleencorp/localizer/model/response/ApiResponse.java
@@ -1,26 +1,3 @@
 package com.fleencorp.localizer.model.response;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public abstract class ApiResponse {
-
-  protected String message;
-
-  @JsonIgnore
-  public abstract String getMessageCode();
-
-  @JsonIgnore
-  public Object[] getParams() {
-    return new Object[] {};
-  }
-
-  @JsonProperty("message")
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(final String message) {
-    this.message = message;
-  }
-}
+public abstract class ApiResponse extends LocalizedResponse { }
---
+++ src/main/java/com/fleencorp/localizer/model/response/LocalizedResponse.java
@@ -0,0 +1,26 @@
+package com.fleencorp.localizer.model.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public abstract class LocalizedResponse {
+
+  protected String message;
+
+  @JsonIgnore
+  public abstract String getMessageCode();
+
+  @JsonIgnore
+  public Object[] getParams() {
+    return new Object[] {};
+  }
+
+  @JsonProperty("message")
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(final String message) {
+    this.message = message;
+  }
+}
